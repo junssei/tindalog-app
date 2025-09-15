014251f66d2480f648afd248923c78fe9b01bf3e
juvncent <junevincentmagsayofernandez@gmail.com>
2025-09-15 12:54:31 +0800

    OnboardingScreen

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -1,3 +1,4 @@
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text } from 'react-native'
 import React from 'react'
 
---
+++ src/screens/intro/OnBoardingScreen.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets, } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OnBoardingScreen = () => {
+  return (
+    <View>
+      <Text>OnBoardingScreen</Text>
+    </View>
+  )
+}
+
+export default OnBoardingScreen
