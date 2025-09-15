295eb7114d7365c03bdd23a02a65afc7504661a0
juvncent <junevincentmagsayofernandez@gmail.com>
2025-09-15 14:51:13 +0800

    Developing: OnBoardingScreen

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+const IMAGES = {
+    NOTEBOOK: require('./notebook.png'),
+    STORE: require('./store.ph'),
+    COIN: require('./coin.png'),
+    LOGO1: require('./TindaLog.png'),
+    LOGO2: require('./tindalog_logo2.png'),
+}
+
+export default IMAGES;
--- src/screens/intro/OnBoardingScreen.tsx
+++ src/screens/intro/OnBoardingScreen.tsx
@@ -1,13 +1,96 @@
 import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets, } from 'react-native-safe-area-context';
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Image, Platform, TouchableOpacity, StyleSheet, Touchable } from 'react-native'
+import { createStaticNavigation, useNavigation, } from '@react-navigation/native';
+import React, { useEffect, useRef, useState } from 'react'
+
+import COLORS from '../../constants/colors';
+import FONTS from '../../constants/fonts';
+
+const board = [
+    {
+        KEY: 1,
+        images: require('../../assets/images/coin.png'),
+        description: 'Welcome to TindaLog—your digital tindahan logbook.' 
+    },
+    {
+        KEY: 2,
+        images: require('../../assets/images/notebook.png'),
+        description: 'Easily track sales, utang, and payments per customer.' 
+    },
+    {
+        KEY: 3,
+        images: require('../../assets/images/store.png'),
+        description: 'Simple, friendly, and made for our community stores.' 
+    },
+]
+
+const Render = () => {
+    const [page, setPage] = useState(0);
+
+    return(
+        <View>
+            {/* <Image source={}/> */}
+            <Text> {board[page].description} </Text>
+            <TouchableOpacity>
+            </TouchableOpacity>
+        </View>
+    )
+}
 
 const OnBoardingScreen = () => {
-  return (
-    <View>
-      <Text>OnBoardingScreen</Text>
-    </View>
-  )
+
+    const navigation = useNavigation();
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <Render/>
+                <View style={{
+                    width: "100%",
+                    height: "100%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                    <TouchableOpacity style={[
+                        styles.button, 
+                        styles.primaryButton
+                    ]}
+                    onPress={() => {
+                        navigation.navigate("WELCOME");
+                    }}>
+                        <Text style={[
+                        styles.primaryButtonText
+                        ]}> Get Started </Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    )
 }
+
+const styles = StyleSheet.create({
+  primaryButton:{
+    backgroundColor: COLORS.PRIMARY,
+  },
+  primaryButtonText:{
+    fontFamily: FONTS.SEMIBOLD,
+    color: COLORS.DARK,
+    fontSize: 20,
+  },
+  secondaryButton:{
+    backgroundColor: COLORS.DARK,
+  },
+  SecondaryButtonText:{
+    fontFamily: FONTS.SEMIBOLD,
+    color: COLORS.WHITE,
+    fontSize: 20,
+  },
+  
+  button:{
+    height: 50,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
 
 export default OnBoardingScreen
